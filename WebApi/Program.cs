75447682c0d3e03f830b084f217286cc32398915
alionser <smirnoviksa@gmail.com>
2022-09-14 17:36:50 +0500

    Add Basic CRUD WebAPI

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+List<Person> users = new List<Person>
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+ 
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+ 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+ 
+app.MapGet("/api/users", ()=> users);
+ 
+app.MapGet("/api/users/{id}", (string id) =>
+{
+    Person? user = users.FirstOrDefault(u => u.Id == id);
+    if (user == null)  return Results.NotFound(new { message = "Пользователь не найден" });
+ 
+    return Results.Json(user);
+});
+ 
+app.MapDelete("/api/users/{id}", (string id) =>
+{
+    Person? user = users.FirstOrDefault(u => u.Id == id);
+ 
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+ 
+    users.Remove(user);
+    return Results.Json(user);
+});
+ 
+app.MapPost("/api/users", (Person user)=>{
+ 
+    user.Id = Guid.NewGuid().ToString();
+    users.Add(user);
+    return user;
+});
+ 
+app.MapPut("/api/users", (Person userData) => {
+ 
+    var user = users.FirstOrDefault(u => u.Id == userData.Id);
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+     
+    user.Age = userData.Age;
+    user.Name = userData.Name;
+    return Results.Json(user);
+});
+ 
 app.Run();
+ 
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
